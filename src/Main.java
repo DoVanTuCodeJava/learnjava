cfa58d8d86a245b77a45ab9c42b56c062214f906
LAB1-PC04\PC1 <dof6589@gmail.com>
2023-05-18 16:24:27 +0700

    sửa test

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		PersonAccount pa = new PersonAccount(1203, null, 1000, 20);
 		pa.withdraw(500);
-		System.out.println("tài khoản còn"+pa.balance);
+		System.out.println("tài khoản còn số dư là: "+pa.balance);
 		SavingAccount sa = new SavingAccount();
 		sa.setBalance(5000000);
 		sa.checkInterest(5);
